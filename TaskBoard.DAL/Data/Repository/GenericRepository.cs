4d1ae3ec37cbcfcc21f46185a24f9e00385e78ef
DartGys <boda20043@gmail.com>
2024-04-25 20:39:10 +0300

    update method created in GenericRepository

--- TaskBoard.DAL/Data/Repository/GenericRepository.cs
+++ TaskBoard.DAL/Data/Repository/GenericRepository.cs
@@ -4,7 +4,7 @@ using TaskBoard.DAL.Interfaces.Repository;
 
 namespace TaskBoard.DAL.Data.Repository;
 
-public class GenericRepository<T> : IGenericRepository<T> where T : class
+public class GenericRepository<T> : IGenericRepository<T> where T : class, IEntity
 {
     private readonly ApplicationDbContext _context;
     
@@ -16,6 +16,19 @@ public class GenericRepository<T> : IGenericRepository<T> where T : class
     public async Task<T?> GetById(Guid id, CancellationToken cancellationToken = default(CancellationToken))
     {
         return await _context.Set<T>().FindAsync(id, cancellationToken);
+    }
+    
+    public async Task<T?> GetById(Guid id, CancellationToken cancellationToken = default(CancellationToken),
+        params Expression<Func<T, object>>[] includes)
+    {
+        IQueryable<T> query = _context.Set<T>();
+
+        foreach (var include in includes)
+        {
+            query = query.Include(include);
+        }
+
+        return await query.FirstOrDefaultAsync(x => x.Id == id, cancellationToken);
     }
 
     public async Task<IEnumerable<T>> GetAllAsync(CancellationToken cancellationToken = default(CancellationToken))
@@ -35,7 +48,8 @@ public class GenericRepository<T> : IGenericRepository<T> where T : class
         foreach(var include in includes)
             query = query.Include(include);
 
-        return await query.ToListAsync(cancellationToken);    }
+        return await query.ToListAsync(cancellationToken);    
+    }
 
     public async Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate, CancellationToken cancellationToken = default(CancellationToken))
     {
@@ -77,6 +91,11 @@ public class GenericRepository<T> : IGenericRepository<T> where T : class
         await _context.Set<T>().AddRangeAsync(entities, cancellationToken);
     }
 
+    public void Update(T entity)
+    {
+        _context.Set<T>().Update(entity);
+    }
+
     public void Remove(T entity)
     {
         _context.Set<T>().Remove(entity);
@@ -86,4 +105,9 @@ public class GenericRepository<T> : IGenericRepository<T> where T : class
     {
         _context.Set<T>().RemoveRange(entities);
     }
+}
+
+public interface IEntity
+{
+    Guid Id { get; set; }
 }
