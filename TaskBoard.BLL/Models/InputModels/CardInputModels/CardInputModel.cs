510e6b6586d32a577f4b790c99e7e9d44c092c82
DartGys <boda20043@gmail.com>
2024-04-30 16:28:34 +0300

    card input fix

--- TaskBoard.BLL/Models/InputModels/CardInputModels/CardInputModel.cs
+++ TaskBoard.BLL/Models/InputModels/CardInputModels/CardInputModel.cs
@@ -6,7 +6,7 @@ namespace TaskBoard.BLL.Models.InputModels;
 
 public class CardInputModel : IMapWith<Card>
 {
-    public Guid Id { get; set; }
+    public string Id { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
     public DateTime CreatedDate { get; private set; } = DateTime.UtcNow;
@@ -16,6 +16,7 @@ public class CardInputModel : IMapWith<Card>
     
     public void Mapping(Profile profile)
     {
-        profile.CreateMap<CardInputModel, Card>();
+        profile.CreateMap<CardInputModel, Card>()
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => string.IsNullOrEmpty(src.Id) ? Guid.Empty : new Guid(src.Id)));
     }
 }
