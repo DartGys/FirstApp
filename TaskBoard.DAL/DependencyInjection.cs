ef13698c322c450d5574dda6cc0a6ef447be64b0
DartGys <boda20043@gmail.com>
2024-05-14 12:32:31 +0300

    dependency

--- TaskBoard.DAL/DependencyInjection.cs
+++ TaskBoard.DAL/DependencyInjection.cs
@@ -16,6 +16,7 @@ public static class DependencyInjection
         services.AddScoped<ICardListRepository, CardListRepository>();
         services.AddScoped<IHistoryLogRepository, HistoryLogRepository>();
         services.AddScoped<IPriorityRepository, PriorityRepository>();
+        services.AddScoped<IBoardRepository, BoardRepository>();
         services.AddScoped<IUnitOfWork, UnitOfWork>();
         
         string connectionString = configuration.GetConnectionString("DefaultConnection");
